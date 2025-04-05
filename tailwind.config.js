fcb1078cdda89396bf33936e45c108a74753a800
ali <fathisut@gmail.com>
2025-04-05 04:45:23 +0330

    starting dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    darkMode: 'class', // ✅ this is the key fix
+    content: ['./**/*.html', './**/*.js'], // or your actual file paths
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
